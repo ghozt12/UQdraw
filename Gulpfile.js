7ae47ecd83e5d5f9c474202e7c236f12179d1f49
Rhain Dodd <rhaindodd@hotmail.com>
2015-08-17 20:35:45 +1000

    fixing inital folder structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,73 +1,47 @@
 var gulp = require('gulp');
 var scss = require('gulp-sass');
 var minifyCSS = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var coffee = require("gulp-coffee");
 var uglify = require("gulp-uglify");
 var concat = require("gulp-concat");
-var clean = require("gulp-clean");
+var del = require('del');
+var concat_css = require("gulp-concat-css");
 
-// TASKS
+// Tasks
 
-	// -- scss
-	// Converts scss and makes a min and normal css file
-    // Also concats any css
-gulp.task('styles', function() {
-	gulp.src('scss/**/*.scss')
-    	.pipe(scss().on('error', scss.logError))
-        .pipe(gulp.dest('./css/'))    
+// (1) Convert SCSS
+gulp.task('sass-to-css', function() {
+    gulp.src('css/scss/**/*.scss')
+        .pipe(scss())
+        .pipe(gulp.dest('./css/'));    
 });
 
-gulp.task('concatCSS', function() {
-    gulp.src('css/**/*.css')
-        .pipe(concat('concat.js'))
-        .pipe(minifyCSS())
-        .pipe(rename('main.min.css'))
-        .pipe(gulp.dest('./css/'));
+// I will fix this up later -rhain
+/* (2) Compress CSS
+gulp.task('css-minify', function() {
+  return gulp.src('css/*.css')
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist'));
 });
 
-	// -- COFFEE SCRIPT
-	// Converts the coffeescript to js
-gulp.task('compile-coffee', function () {
-    gulp.src('./coffee/*.coffee') // path to your file
-    .pipe(coffee())
-    .pipe(uglify())
-    .pipe(rename('main.min.js'))
-    .pipe(gulp.dest('./js/')); // Destination of min file
-});
-
-    // -- COPY
-    // Sends all files to the dist folder 
+// (3) Create Dist folder
 gulp.task('copy', function() {
     // Copy html
     gulp.src('index.html')
         .pipe(gulp.dest('dist/'));
 
-    // Copy js
-    gulp.src('js/*.min.js')
+    // Copy Assets
+    gulp.src('assets/**', {cwd: './'})
         .pipe(gulp.dest('dist/'));
-
-    // Copy css
-    gulp.src('css/*.min.css')
-        .pipe(gulp.dest('dist/'));
+    
+    // Copy all HTML pages
+    gulp.src('pages/*.html')
+        .pipe(gulp.dest('dist/pages/'));
 });
+*/
+// (4) Watch
+// gulp.task('production', ['sass-to-css', 'css-minify', 'copy']);
 
-// Delete the dist directory
-gulp.task('clean', function() {
-    return gulp.src('dist/')
-        .pipe(clean());
-});
-
-	// -- DEFAULT
-	// Type watch in the terminal and it will automatically convert scss to css
+// (5) Default
 gulp.task('default', function() {
-    gulp.watch('scss/**/*.scss',['styles', 'concatCSS', 'copy']);
-    gulp.watch('*.html',['copy']);
-    gulp.watch('coffee/**/*.coffee',['compile-coffee', 'copy']);
+    gulp.watch('css/scss/**/*.scss',['sass-to-css']);
 });
-
-    // -- PRODUCTION
-    // -- DEFAULT
-    // Type watch in the terminal and it will automatically convert scss to css
-gulp.task('production', ['styles', 'concatCSS', 'compile-coffee', 'copy']);
-
