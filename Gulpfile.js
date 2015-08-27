9fee332856c2087b72cd8f0ef088725030e304bb
Rhain Dodd <rhaindodd@hotmail.com>
2015-08-27 11:03:25 +1000

    added compass for scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -14,36 +14,10 @@ gulp.task('sass-to-css', function() {
     gulp.src('css/scss/**/*.scss')
         .pipe(scss())
         .pipe(autoprefixer())
+        .pipe(concat('main.css'))
         .pipe(gulp.dest('./css/'));    
 });
 
-// I will fix this up later -rhain
-/* (2) Compress CSS
-gulp.task('css-minify', function() {
-  return gulp.src('css/*.css')
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist'));
-});
-
-// (3) Create Dist folder
-gulp.task('copy', function() {
-    // Copy html
-    gulp.src('index.html')
-        .pipe(gulp.dest('dist/'));
-
-    // Copy Assets
-    gulp.src('assets/**', {cwd: './'})
-        .pipe(gulp.dest('dist/'));
-    
-    // Copy all HTML pages
-    gulp.src('pages/*.html')
-        .pipe(gulp.dest('dist/pages/'));
-});
-*/
-// (4) Watch
-// gulp.task('production', ['sass-to-css', 'css-minify', 'copy']);
-
-// (5) Default
 gulp.task('default', function() {
     gulp.watch('css/scss/**/*.scss',['sass-to-css']);
 });
