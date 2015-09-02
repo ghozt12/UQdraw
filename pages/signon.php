70375882e93d9154c7022fbbcd42f614b726f832
Devin Marriott <devin.marriott@student.uq.edu.au>
2015-09-02 16:10:28 +1000

    signon.php strictly to redirect staff to lecturer-mode.html and to display students name 'Welcome ...'. Removed DB processes.

--- pages/signon.php
+++ pages/signon.php
@@ -1,10 +1,6 @@
 <?php
-	include 'conn.php';
-	session_start();
-	$userID = $_SERVER['HTTP_X_UQ_USER'];
 	$userType = $_SERVER['HTTP_X_UQ_USER_TYPE'];
 	$namejson = $_SERVER['HTTP_X_KVD_PAYLOAD'];
-	$existquery = "INSERT INTO Lecturer VALUES ('$userID')";
 	$namearray = json_decode($namejson);
 
 	if($userType == "Student")
@@ -13,13 +9,6 @@
 	}
 	if($userType == "Staff")
 	{
-		if(!mysqli_query($conn, $existquery))
-		{
-			echo mysqli_error($conn);
-		}
-		else
-		{
-			echo "<script> window.location.href = 'http://teamone.uqcloud.net/pages/lecture-mode.html'</script>";	
-		}
+		echo "<script> window.location.href = 'http://teamone.uqcloud.net/pages/lecture-mode.html'</script>";
 	}		
 ?>
