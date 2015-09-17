0a9655be81a50b9bc980ebb07f839eab837b3325
Devin Marriott <devin.marriott@student.uq.edu.au>
2015-09-17 12:14:22 +1000

    can delete question now :)

---
+++ uqDrawBackend/deleteQuestion.php
@@ -0,0 +1,14 @@
+<?php
+
+include "connectDB.php";
+$questionID = $_POST["questionID"];
+
+$query = "DELETE FROM `Question` WHERE questionID = ".$questionID;
+$result = mysqli_query($mysqli, $query);
+if($result){
+	echo "successfully deleted";
+}
+
+
+
+?>
