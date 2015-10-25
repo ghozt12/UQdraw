9a84065cd914ffda23084c042c95de9feaf83a1b
Devin Marriott <devin.marriott@student.uq.edu.au>
2015-10-26 00:44:39 +1000

    Can edit questions now

---
+++ uqDrawBackend/editQuestion.php
@@ -0,0 +1,18 @@
+<?php
+
+include "connectDB.php";
+$questionID = $_POST["questionID"];
+$questionWeek = $_POST["questionWeek"];
+$title = $_POST["title"];
+
+
+$query = "UPDATE `Question` SET `questionWeek`=".$questionWeek.", `questionTitle`='".$title."' WHERE `questionID` = ".$questionID;
+$result = mysqli_query($mysqli, $query);
+
+//if($result){
+//	echo "successfully deleted";
+//}
+
+$mysqli->close();
+
+?>
